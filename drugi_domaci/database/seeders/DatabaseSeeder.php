9233ea57575d35642b262d2acd4ee495e02aa3ea
dusicasujdovic <dusicasujdovic00@gmail.com>
2023-08-23 12:16:09 +0200

    Kreirani seederi

--- drugi_domaci/database/seeders/DatabaseSeeder.php
+++ drugi_domaci/database/seeders/DatabaseSeeder.php
@@ -2,17 +2,61 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Genre;
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Genre::truncate();
+        Author::truncate();
+        Book::truncate();
+
+        $user1=User::factory()->create();
+        $user2=User::factory()->create();
+        $user3=User::factory()->create();
+
+        $genre1=Genre::create(['name'=>'Romance novel','description'=>'A romance novel generally refers to a type of genre fiction novel which places its primary focus on the relationship and romantic love between two people, and usually has an "emotionally satisfying and optimistic ending."']);
+        $genre2=Genre::create(['name'=>'Action fiction','description'=>'Action fiction is a literary genre that focuses on stories that involve high-stakes, high-energy, and fast-paced events.']);
+        $genre3=Genre::create(['name'=>'Science fiction','description'=>'Science fiction is a genre of speculative fiction, which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life.']);
+
+        $author1=Author::create(['first_name'=>' André','last_name'=>'Aciman']);
+        $author2=Author::create(['first_name'=>'Veronica','last_name'=>'Roth']);
+        $author3=Author::create(['first_name'=>'Frank','last_name'=>'Herbert']);
+
+         $book1=Book::create([
+            'title'=>'Call me by your name',
+            'number of pages'=>256,
+            'genre_id'=>$genre1->id,
+            'author_id'=>$author1->id,
+            'user_id'=>$user1->id,
+            'year of release'=>2007
+         ]);
+         $book2=Book::create([
+            'title'=>'Divergent',
+            'number of pages'=>487,
+            'genre_id'=>$genre2->id,
+            'author_id'=>$author2->id,
+            'user_id'=>$user2->id,
+            'year of release'=>2011
+         ]);
+         $book3=Book::create([
+            'title'=>'Dina',
+            'number of pages'=>704,
+            'genre_id'=>$genre3->id,
+            'author_id'=>$author3->id,
+            'user_id'=>$user3->id,
+            'year of release'=>1965
+         ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
